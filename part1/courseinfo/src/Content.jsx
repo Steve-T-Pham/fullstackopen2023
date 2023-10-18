142f9b74bb00155c64b29f18c97125e1caba8c41
unknown <stp40@case.edu>
2023-10-17 20:43:08 -0400

    project hierarchy initilization

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,13 @@
+import Part from './Part';
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            <Part part={parts[0].name} exercises={parts[0].exercises}/>
+            <Part part={parts[1].name} exercises={parts[1].exercises}/>
+            <Part part={parts[2].name} exercises={parts[2].exercises}/>
+        </div>
+    )
+}
+
+export default Content;
