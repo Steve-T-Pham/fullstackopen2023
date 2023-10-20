04888cfe1482202d8cce5113e4f75396b3b2e674
unknown <stp40@case.edu>
2023-10-20 19:52:14 -0400

    courseinfo completion + phonebook initialization

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -2,13 +2,16 @@ import Part from "./Part";
 
 const Content = ({ parts }) => {
 
-    const total = parts.reduce((s, p) => console.log(s.exercises))
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
 
     return(
-        <ul>
-            {parts.map(part => <Part part={part} key={part.id} />)}
-        </ul>
-    );
+        <div>
+            <ul>
+                {parts.map(part => <Part part={part} key={part.id} />)}
+            </ul>
+            <p><strong>total of {total} exercises</strong></p>
+        </div>
+        );
 }
 
 export default Content;
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  }
+
+  const addName = (e) => {
+    e.preventDefault();
+    const newPersonObject = {
+      name: newName
+    }
+    setPersons(persons.concat(newPersonObject));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
