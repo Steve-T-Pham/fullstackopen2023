61f9e504fdf75d3ec5de44d02c24a930e3df4cb1
unknown <stp40@case.edu>
2023-10-23 20:11:07 -0400

    filter feature added phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])    
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  }
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
   }
 
   const addName = (e) => {
@@ -15,10 +28,13 @@ const App = () => {
     const isDuplicate = persons.find(person => person.name === newName);
     if (!isDuplicate){
       const newPersonObject = {
-        name: newName
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(newPersonObject));
       setNewName('');
+      setNewNumber('');
     }
     else
       alert(`${newName} is already added to phonebook`);
@@ -27,20 +43,27 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input onChange={handleFilterChange} value={filter} />
+      <h1>add a new</h1>
+
       <form onSubmit={addName}>
         <div>
-          name: <input onChange={handleNameChange} value={newName}/>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {(!filter ? persons.map(person => <li key={person.id}>{person.name} {person.number}</li>) : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => <li key={person.id}>{person.name} {person.number}</li>))}
       </ul>
     </div>
   )
 }
 
-export default App
+export default App;
