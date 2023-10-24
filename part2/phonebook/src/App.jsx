dc08e673f898e2155c5941d2bc2676ecdf0d61fd
unknown <stp40@case.edu>
2023-10-24 00:13:03 -0400

    axios and json-server to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import PersonForm from './PersonForm';
 import Filter from './Filter';
 import Persons from './Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])    
+  const [persons, setPersons] = useState([])    
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
@@ -42,6 +38,14 @@ const App = () => {
     else
       alert(`${newName} is already added to phonebook`);
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        setPersons(res.data);
+      })
+  }, []);
 
   return (
     <div>
